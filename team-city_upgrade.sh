7c0ed9e33350d1d8314592fb82e71be60b3d288e
Linux User <aituar-s@auto.kz>
2023-05-29 12:09:05 +0600

    script logic update

--- team-city_upgrade.sh
+++ team-city_upgrade.sh
@@ -1,20 +1,13 @@
 #!/bin/sh
 SCRIPT_HOME="$HOME/scripts/team-city_upgrade"
 TEAMCITY_HOME="$HOME/team-city"
+[ -f ${SCRIPT_HOME}/.env ] && . ${SCRIPT_HOME}/.env
 TEAMCITY_URL="http://localhost:8111"
-<<<<<<< HEAD
 LOG_FILE="${SCRIPT_HOME}/team-city_upgrade.log"
 #token_teamcity="insert your TeamCity token here, or to .env file"
 VERSION_CURRENT=$(curl -H "Authorization: Bearer $token_teamcity" -H "Content-Type: application/xml"  -X GET "$TEAMCITY_URL/admin/admin.html?item=update" 2>/dev/null | grep -Eio  ".{0,10}\(build.{0,8}" | tail -n 1 )
 IS_NEW_VERSION=$(curl -H "Authorization: Bearer $token_teamcity" -H "Content-Type: application/xml"  -X GET  "$TEAMCITY_URL/admin/admin.html?item=update" 2>/dev/null | grep -Eio  "updateOptionsContainer")
 VERSION_NEW=$(curl -H "Authorization: Bearer $token_teamcity" -H "Content-Type: application/xml"  -X GET "$TEAMCITY_URL/admin/admin.html?item=update" 2>/dev/null | grep -Eio -m 1 ".{0,10}\(build.{0,8}") 
-=======
-#TEAMCITY_TOKEN = "insert your Team-City token here"
-LOG_FILE="${SCRIPT_HOME}/team-city_upgrade.log"
-VERSION_CURRENT=$(curl -H "Authorization: Bearer $TEAMCITY_TOKEN" -H "Content-Type: application/xml"  -X GET "$TEAMCITY_URL/admin/admin.html?item=update" 2>/dev/null | grep -Eio  ".{0,10}\(build.{0,8}" | tail -n 1 )
-IS_NEW_VERSION=$(curl -H "Authorization: Bearer $TEAMCITY_TOKEN" -H "Content-Type: application/xml"  -X GET  "$TEAMCITY_URL/admin/admin.html?item=update" 2>/dev/null | grep -Eio  "updateOptionsContainer")
-VERSION_NEW=$(curl -H "Authorization: Bearer $TEAMCITY_TOKEN" -H "Content-Type: application/xml"  -X GET "$TEAMCITY_URL/admin/admin.html?item=update" 2>/dev/null | grep -Eio -m 1 ".{0,10}\(build.{0,8}") 
->>>>>>> 192565beda6f39ee7154548c30ce3a474ec750a8
 IMAGE_ID_CURRENT=$(docker images -q "jetbrains/teamcity-server")
 #TG_TOKEN_ADMIN="insert your Telegram Admin Chat-ID here, or to .env file"
 #TG_TOKEN_GROUP="insert your Telegram Group Chat-ID here, or to .env file"
@@ -40,15 +33,10 @@ if [[ $IS_NEW_VERSION ]]
 	docker compose up -d
 
         # Get authentication token from Log file to unlcok Team-City after upgrade: 
-	TEAMCITY_UNLOCK_TOKEN=$(grep -Eo '\d{19}' ${TEAMCITY_HOME}/server/logs/teamcity-server.log | tail -n 1) 
+	tc_unlock_token=$(grep -Eo '\d{19}' ${TEAMCITY_HOME}/server/logs/teamcity-server.log | tail -n 1) 
 	# Send unlock token to log-file and Telegram:
-<<<<<<< HEAD
 	echo -e "\e[36mSuper user authentication token: ${tc_unlock_token}\e[0m"
 	/usr/bin/telegram-send "Super user authentication token: ${tc_unlock_token}" ${TG_TOKEN_ADMIN} >/dev/null
-=======
-	echo -e "\e[36mSuper user authentication token: ${TEAMCITY_UNLOCK_TOKEN}\e[0m"
-	/usr/bin/telegram-send "Super user authentication token: ${TEAMCITY_UNLOCK_TOKEN}" >/dev/null
->>>>>>> 192565beda6f39ee7154548c30ce3a474ec750a8
 
 	# Report upgrade completion to log-file and Telegram:
 	echo -e "\e[32mTeam-City successfully upgraded to version: $VERSION_NEW\e[0m" && \
